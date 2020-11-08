16c11e7aa94d5013d62fd6d9973b9c5eca095e06
giulianaarce <arceg45@gmail.com>
2020-11-08 18:07:44 -0300

    Updates

---
+++ precache-manifest.169de36f2be61d4ea381a541797a0630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fef534c3ede38a1139e5c45e075314f",
+    "url": "/prueba-react/index.html"
+  },
+  {
+    "revision": "7edca81429f7bb9e4b09",
+    "url": "/prueba-react/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e5a26e8f56211a948528",
+    "url": "/prueba-react/static/js/2.0938dda7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/prueba-react/static/js/2.0938dda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edca81429f7bb9e4b09",
+    "url": "/prueba-react/static/js/main.78f0ec6c.chunk.js"
+  },
+  {
+    "revision": "6086927a690a0996f890",
+    "url": "/prueba-react/static/js/runtime-main.bf1ef870.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/prueba-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
